634255c01ad4adf4cfb129d976b0dbf6e84d8698
Jyri Nieminen <nieminen.jyri.j@gmail.com>
2026-06-26 10:42:38 +0300

    Part 1 - Exercise 1.7
    Submission for exercises
    - 1.7: unicafe step2

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,15 +1,40 @@
 import { useState } from 'react'
 
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+  const all = good + neutral + bad
+  
+const average = all === 0 ? 0 : (good - bad) / all
+const positive = all === 0 ? 0 : (good / all) * 100
+
   return (
-    <div>
-      code here
-    </div>
+    <>
+      <div>
+        <h1>give feedback</h1>
+        <Button onClick={handleGoodClick} text='good' />
+        <Button onClick={handleNeutralClick} text='neutral' />
+        <Button onClick={handleBadClick} text='bad' />
+      </div>
+      <div>
+        <h2>statistics</h2>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average}</p>
+        <p>positive {positive} %</p>
+      </div>
+    </>
   )
 }
 
